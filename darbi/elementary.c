5d8988d1fef57b8f4314e9f8676fa9b8bf509d8e
user <daniksurmach@gmail.com>
2019-09-19 16:02:27 +0300

    20190919_16_02

---
+++ darbi/elementary.c
@@ -0,0 +1,5 @@
+void main(){}
+//Šis ir mans pirmais kods C vālodā!
+/*Komentāra 1. rinda
+Komentāra 2.rinda
+Komentāra 3.rinda */
---
+++ darbi/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+ {
+//"Hello world!";
+//printf("Hello world!"); 
+printf("Hello world!\n");
+ }
