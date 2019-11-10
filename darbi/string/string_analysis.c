044a25e627da6f48cb3e1bfaa0bceb0f81c474b0
daniil172101 <daniksurmach@gmail.com>
2019-11-10 21:45:22 +0200

    10112019_21_45

--- darbi/string/string_analysis.c
+++ darbi/string/string_analysis.c
@@ -4,15 +4,22 @@
 
 void main()
 {
- char str[50];
- int i,x,y;
+ char s[51], str[50];
+ int i,x,y,r,b;
  printf("Lūdzu, ievadiet teikumu : ");
  scanf("%[^\n]s", str);
  for (i= 0;str[i] != '\0';i++){
   if (str[i] == ' ' && str[i+1] != ' ')
    x++;
  }
+ for (i = 0;str[i] != '\0';i++){
+  if (str[i] == ' ' && str[i+1] != ' ')
+   b = i;
+ }
+ if (str[0] == ' ')
+  x--;
  if (str[i-1]!=' ')
   x++;
  printf("Teikumam bija %d vārdi \n",x);
+ printf("Teikumam bija %d vārdi, kuri ir vienādi vai garāki par 5 simboliem \n",y);
 }
