abb2a4ed11ca6d5ccf71d2b39a0b5f85b28942be
daniil172101 <daniksurmach@gmail.com>
2019-11-11 20:49:28 +0200

    11112019_20_50

--- darbi/string/string_analysis.c
+++ darbi/string/string_analysis.c
@@ -2,24 +2,38 @@
 #include<string.h>
 
 
-void main()
+int main()
 {
- char s[51], str[50];
- int i,x,y,r,b;
+ char str[50];
+ long int b,i,x,y;
+ y = b = 0;
  printf("Lūdzu, ievadiet teikumu : ");
  scanf("%[^\n]s", str);
- for (i= 0;str[i] != '\0';i++){
-  if (str[i] == ' ' && str[i+1] != ' ')
+ for (i= 0;str[i] != '\0' ;i++){
+  if (str[i] == ' ' && str[i+1] != ' '){
    x++;
  }
- for (i = 0;str[i] != '\0';i++){
-  if (str[i] == ' ' && str[i+1] != ' ')
-   b = i;
  }
  if (str[0] == ' ')
   x--;
  if (str[i-1]!=' ')
   x++;
+ i= 0;
+while(str[i] != '\0')
+{
+ if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+  ++b;
+ if ((str[i] >= '!' && str[i] <= '/') || (str[i] >= ':' && str[i] <= '@') || (str[i] >= '[' && str[i] <= '`') || (str[i] >= '{' && str[i] <= '~'))
+  b--;
+ if (b == 5)
+  ++y;
+ if (str[i] == ' ')
+  b = 0;
+ ++i;
+}
  printf("Teikumam bija %d vārdi \n",x);
  printf("Teikumam bija %d vārdi, kuri ir vienādi vai garāki par 5 simboliem \n",y);
+ return 0;
 }
+
+
